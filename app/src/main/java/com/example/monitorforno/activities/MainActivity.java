9f85bdc00bcc461d4220a0d8f76fce931e78e36c
ArturDCosta <arturdenicolcosta@gmail.com>
2026-06-10 13:48:18 -0300

    Primeiro commit do projeto

---
+++ app/src/main/java/com/example/monitorforno/activities/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.monitorforno.activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.example.monitorforno.R;
+import com.example.monitorforno.fragments.AlertasFragment;
+import com.example.monitorforno.fragments.DashboardFragment;
+import com.example.monitorforno.fragments.HistoricoFragment;
+import com.example.monitorforno.fragments.PerfilFragment;
+import com.example.monitorforno.fragments.TemporizadoresFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+
+        // Fragment inicial
+        replaceFragment(new DashboardFragment());
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            int id = item.getItemId();
+
+            if (id == R.id.nav_dashboard) {
+                replaceFragment(new DashboardFragment());
+
+            } else if (id == R.id.nav_historico) {
+                replaceFragment(new HistoricoFragment());
+
+            } else if (id == R.id.nav_alertas) {
+                replaceFragment(new AlertasFragment());
+
+            } else if (id == R.id.nav_temporizadores) {
+                replaceFragment(new TemporizadoresFragment());
+
+            } else if (id == R.id.nav_perfil) {
+                replaceFragment(new PerfilFragment());
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
+    }
+}
