4af38431a80c30c1b39fe62ea060927cb14bcb2f
ArturDCosta <arturdenicolcosta@gmail.com>
2026-06-10 15:27:48 -0300

    Cor nos graus e icones

--- app/src/main/java/com/example/monitorforno/fragments/DashboardFragment.java
+++ app/src/main/java/com/example/monitorforno/fragments/DashboardFragment.java
@@ -1,5 +1,6 @@
 package com.example.monitorforno.fragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import androidx.fragment.app.Fragment;
 
 import com.example.monitorforno.R;
+import com.google.android.material.card.MaterialCardView;
 
 public class DashboardFragment extends Fragment {
 
@@ -17,10 +19,44 @@ public class DashboardFragment extends Fragment {
             ViewGroup container,
             Bundle savedInstanceState) {
 
-        return inflater.inflate(
+        View view = inflater.inflate(
                 R.layout.fragment_dashboard,
                 container,
                 false
         );
+
+        MaterialCardView cardEstadoForno =
+                view.findViewById(R.id.cardEstadoForno);
+
+        String estado = "FORNO_AQUECENDO";
+
+        switch (estado) {
+
+            case "FORNO_AQUECENDO":
+                cardEstadoForno.setCardBackgroundColor(
+                        getResources().getColor(R.color.forno_aquecendo)
+                );
+                break;
+
+            case "FORNO_DESLIGADO":
+                cardEstadoForno.setCardBackgroundColor(
+                        getResources().getColor(R.color.forno_desligado)
+                );
+                break;
+
+            case "FORNO_ATIVO":
+                cardEstadoForno.setCardBackgroundColor(
+                        getResources().getColor(R.color.forno_ativo)
+                );
+                break;
+
+            case "FORNO_ESFRIANDO":
+                cardEstadoForno.setCardBackgroundColor(
+                        getResources().getColor(R.color.forno_esfriando)
+                );
+                break;
+        }
+
+        return view;
     }
 }
