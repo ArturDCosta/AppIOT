44448ec5134b9b5e48f7bc2577ef8fbe0f5b9090
ArturDCosta <arturdenicolcosta@gmail.com>
2026-06-25 17:01:45 -0300

    url do servidor

--- app/src/main/java/com/example/monitorforno/network/RetrofitClient.java
+++ app/src/main/java/com/example/monitorforno/network/RetrofitClient.java
@@ -16,7 +16,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.0.132:8080/";
+    private static final String BASE_URL = "http://56.125.180.47:8080/";
     private static Retrofit instance;
 
     private static Retrofit getInstance(Context context) {
