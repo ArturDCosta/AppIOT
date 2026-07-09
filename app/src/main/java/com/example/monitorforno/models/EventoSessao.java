786215bb4fe89413c15d8055360fd9c68bd90c3a
ArturDCosta <arturdenicolcosta@gmail.com>
2026-07-09 17:01:25 -0300

    eventos da sessao ajustados

--- app/src/main/java/com/example/monitorforno/models/EventoSessao.java
+++ app/src/main/java/com/example/monitorforno/models/EventoSessao.java
@@ -1,15 +1,28 @@
 package com.example.monitorforno.models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class EventoSessao {
 
+    // Diz ao Gson: "O JSON vai mandar 'tipo', mas guarde na variável 'descricao'"
+    @SerializedName("tipo")
     private String descricao;
+
+    // Diz ao Gson: "O JSON vai mandar 'criadoEm', mas guarde na variável 'horario'"
+    @SerializedName("criadoEm")
     private String horario;
 
-    public EventoSessao(String descricao) {
+    public EventoSessao(String descricao, String horario) {
         this.descricao = descricao;
+        this.horario = horario;
     }
 
     public String getDescricao() {
         return descricao;
+    }
+
+    // Adicionado o Getter do horário para você poder usar no Adapter e mostrar na tela!
+    public String getHorario() {
+        return horario;
     }
 }
